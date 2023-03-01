e6c3d6af9a72f5c66b2638097f6cacfa9e192042
Nishant Aggarwal <nishant@aspiredu.com>
2023-03-01 15:14:39 -0500

    Script working to set up EC2 instance with mounted volume, appropriate filesystem, and script code and dependencies with Terraform

--- bin/ec2_setup.sh
+++ bin/ec2_setup.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 sudo apt update
 sudo apt install -y unzip python3-pip
@@ -12,12 +11,12 @@ sudo mkdir /home/ubuntu/data/
 echo "Mounting Attached Volume"
 sudo mount /dev/nvme1n1 /home/ubuntu/data/
 echo "Creating new directories"
-sudo mkdir /home/ubuntu/data/CrunchyBackups/
-cd /home/ubuntu/data/CrunchyBackups/
+sudo mkdir /home/ubuntu/data/CrunchyBackupsData/
+cd /home/ubuntu/data/CrunchyBackupsData/
 sudo mkdir aspiredu-au aspiredu-ms aspiredu-prd-a aspiredu-prd-b aspiredu-prd-c aspiredu-prd-d aspiredu-prd-e aspiredu-prd-g aspiredu-prd-h aspiredu-prd-i aspiredu-prd-j aspiredu-prd-k aspiredu-prd-l aspiredu-stg aspireprod aspirestaging
 cd /home/ubuntu/
 echo "Cloning GitHub repository"
-git clone https://github.com/aspiredu/heroku-database-backups.git
+git clone https://github.com/aspiredu/crunchy-backups.git
 echo "Installing AWS CLI"
 sudo mkdir installations
 cd installations
@@ -25,5 +24,17 @@ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip
 unzip awscliv2.zip
 sudo ./aws/install
 echo "Installing script dependencies"
-cd /home/ubuntu/heroku-database-backups/
+cd /home/ubuntu/crunchy-backups/
 pip3 install -r requirements.txt
+cat << EOF > ./bin/.env
+CRUNCHY_API_KEY = ""
+CRUNCHY_TEAM_ID = ""
+
+ASPIRE_AWS_ACCESS_KEY_ID = ""
+ASPIRE_AWS_SECRET_ACCESS_KEY = ""
+
+CLUSTERS_TO_BACKUP = "aspiredu-stg"
+
+LOCAL_TEMP_DOWNLOADS_PATH = "/home/ubuntu/data/CrunchyBackupsData/"
+BASE_S3_PREFIX = "crunchybridge/"
+EOF
--- main.tf
+++ main.tf
@@ -7,37 +7,7 @@ resource "aws_instance" "cb_backup" {
   tags = {
     Name = local.name_tag,
   }
-    # user_data = "${file("./bin/ec2_setup.sh")}"
-  user_data = <<EOF
-#!/bin/bash
-sudo apt update
-sudo apt install -y unzip python3-pip
-while ! ls /dev/nvme1n1 > /dev/null
-do 
-    sleep 5
-done
-echo "Creating XFS Filesystem on Attached Volume"
-sudo mkfs -t xfs /dev/nvme1n1
-sudo mkdir /home/ubuntu/data/
-echo "Mounting Attached Volume"
-sudo mount /dev/nvme1n1 /home/ubuntu/data/
-echo "Creating new directories"
-sudo mkdir /home/ubuntu/data/CrunchyBackups/
-cd /home/ubuntu/data/CrunchyBackups/
-sudo mkdir aspiredu-au aspiredu-ms aspiredu-prd-a aspiredu-prd-b aspiredu-prd-c aspiredu-prd-d aspiredu-prd-e aspiredu-prd-g aspiredu-prd-h aspiredu-prd-i aspiredu-prd-j aspiredu-prd-k aspiredu-prd-l aspiredu-stg aspireprod aspirestaging
-cd /home/ubuntu/
-echo "Cloning GitHub repository"
-git clone https://github.com/aspiredu/heroku-database-backups.git
-echo "Installing AWS CLI"
-sudo mkdir installations
-cd installations
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
-echo "Installing script dependencies"
-cd /home/ubuntu/heroku-database-backups/
-pip3 install -r requirements.txt
-EOF
+  user_data = file("./bin/ec2_setup.sh")
 }
 
 resource "aws_ebs_volume" "volume" {
