722ae6863447b35f24efbe8d54e3d8fcee19efd2
Nishant Aggarwal <nishant@aspiredu.com>
2023-03-15 17:04:25 -0400

    Fix path to backend snitch map

--- bin/crunchy_copy.py
+++ bin/crunchy_copy.py
@@ -163,7 +163,7 @@ def summarize(start, finish, download_finishes, upload_finishes):
 
 def signal_dead_mans_snitch():
     backend_snitch_map = {}
-    with open("./backend-snitch-map.json") as json_map:
+    with open("./bin/backend-snitch-map.json") as json_map:
         backend_snitch_map = json.load(json_map)
     res = requests.post(backend_snitch_map[ASPIRE_BACKEND], data={"m": "Completed"})
     return res
