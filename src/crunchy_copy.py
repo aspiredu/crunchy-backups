4491d6dcef4628ffc98bc5a2afe52afc0f149f4c
Tim Schilling <tim@aspiredu.com>
2023-09-29 11:37:09 -0500

    Selectively copy backup information from CrunchyBridge.
    This revises our approach to only copy today's backup rather than
    all ten backups from crunchy bridge to our S3 bucket. It also will
    only copy the info file from the archive directory rather than the
    entire directory. This is because CB should be using the --copy-archive
    option with our backups resulting in the WAL being inserted in the
    actual backup directory.
    
    This adds a --dry-run option which will print out summary information
    and the commands that will be run.
    
    This now requires the cluster to be specified when running since that
    was the default anyway.

--- src/crunchy_copy.py
+++ src/crunchy_copy.py
@@ -9,6 +9,7 @@ from zoneinfo import ZoneInfo
 
 import requests
 import sentry_sdk
+from dateutil.relativedelta import relativedelta
 from dotenv import load_dotenv
 
 from src.s3 import get_s3
@@ -21,9 +22,21 @@ CRUNCHY_TEAM_ID = os.getenv("CRUNCHY_TEAM_ID")
 ASPIRE_AWS_ACCESS_KEY_ID = os.getenv("ASPIRE_AWS_ACCESS_KEY_ID")
 ASPIRE_AWS_SECRET_ACCESS_KEY = os.getenv("ASPIRE_AWS_SECRET_ACCESS_KEY")
 
-LOCAL_TEMP_DOWNLOADS_PATH = os.getenv("LOCAL_TEMP_DOWNLOADS_PATH")
-BASE_S3_PREFIX = os.getenv("BASE_S3_PREFIX")
+LOCAL_TEMP_DOWNLOADS_PATH = os.getenv("LOCAL_TEMP_DOWNLOADS_PATH", "temp/")
+
+# The v2 directory is necessary so that we can more easily delete the other
+# backups when all have been converted to our the format that either includes
+# the WAL in the daily_backup/pg_data/pg_wal directory or the WAL in the
+# archive directory is limited to what is necessary for the specific
+# daily backup. Eventually v2 should be the only thing in the crunchybridge
+# directory.
+S3_BACKUP_DEST_PREFIX = os.getenv("S3_BACKUP_DEST_PREFIX", "crunchybridge/v2/")
 SENTRY_DSN = os.getenv("SENTRY_DSN")
+
+# Recommended values are:
+# testing: "ONEZONE_IA"
+# production: "DEEP_ARCHIVE"
+STORAGE_CLASS = os.getenv("S3_STORAGE_CLASS", "DEEP_ARCHIVE")
 
 STAGING_BACKENDS = ["aspirestaging", "aspiredu-stg"]
 AU_BACKENDS = ["aspiredu-au"]
@@ -47,7 +60,16 @@ def get_crunchy_clusters():
     return json.loads(response.content.decode("utf-8"))["clusters"]
 
 
-def get_cluster_backup_info(cluster_id):
+def three_years_from_now():
+    return datetime.now(TZ) + relativedelta(years=3)
+
+
+def get_cluster_backup_info(cluster_id: str, backup_target: str) -> dict:
+    """Fetch the cluster's backup information from CrunchyBridge.
+
+    This combines the backup-token information with the backup
+    token into a single dictionary.
+    """
     headers = {
         "Authorization": f"Bearer {CRUNCHY_API_KEY}",
     }
@@ -55,18 +77,19 @@ def get_cluster_backup_info(cluster_id):
         f"https://api.crunchybridge.com/clusters/{cluster_id}/backup-tokens",
         headers=headers,
     )
+    backup_tokens.raise_for_status()
     backup_info = requests.get(
         f"https://api.crunchybridge.com/clusters/{cluster_id}/backups"
         "?order=desc&order_field=name",
         headers=headers,
     )
+    backup_info.raise_for_status()
     response = json.loads(backup_tokens.content.decode("utf-8"))
-    if not backup_tokens.status_code == HTTPStatus.OK:
-        backup_tokens.raise_for_status()
-    if not backup_info.status_code == HTTPStatus.OK:
-        backup_info.raise_for_status()
     backup_info = json.loads(backup_info.content.decode("utf-8"))
-    response["backups"] = backup_info["backups"]
+    # Look up the specific backup for the given target.
+    response["backup"] = [
+        backup for backup in backup_info["backups"] if backup["name"].startswith(backup_target)
+    ][0]
     return response
 
 
@@ -84,18 +107,21 @@ def watch_process_logs(process):
             return
 
 
-def upload_all_files_in_dir(source_dir, stanza, bucket, backend_name, prefix=None):
+def upload_all_files_in_dir(source_dir, bucket, prefix):
     print("Uploading files...")
-    if not source_dir.endswith("/"):
-        source_dir = source_dir + "/"
     for root, _, files in os.walk(source_dir):
         for file in files:
             full_path = os.path.join(root, file)
 
             # Set up the file structure for S3
-            new_file_key = f"{prefix}{backend_name}/{full_path[len(source_dir):]}"
+            new_file_key = f"{prefix}/{full_path[len(source_dir):]}"
             print(f"Uploading... {new_file_key}")
-            bucket.upload_file(os.path.join(root, file), new_file_key)
+            bucket.upload_file(
+                os.path.join(root, file),
+                new_file_key,
+                Expires=three_years_from_now(),
+                StorageClass=STORAGE_CLASS,
+            )
     return
 
 
@@ -113,14 +139,6 @@ def delete_all_files_in_dir(source_dir):
         except Exception as e:
             print("Failed to delete %s. Reason: %s" % (file_path, e))
     return
-
-
-def backup_exists(s3, bucket, cluster_name, stanza, backup_name):
-    if not backup_name.endswith("/"):
-        backup_name = backup_name + "/"
-    prefix = f"crunchybridge/{cluster_name}/backup/{stanza}/{backup_name}"
-    resp = s3.list_objects(Bucket=bucket.name, Prefix=prefix, MaxKeys=1)
-    return "Contents" in resp
 
 
 def seconds_to_readable(seconds):
@@ -149,7 +167,9 @@ def signal_dead_mans_snitch(cluster: str):
 
 
 class CrunchyCopy:
-    def __init__(self, bucket_name: str, cluster_name: str, backup_target: str):
+    def __init__(
+        self, bucket_name: str, cluster_name: str, backup_target: str, dry_run: bool = False
+    ):
         """
 
         :param bucket_name: The name of the bucket to copy to.
@@ -159,43 +179,43 @@ class CrunchyCopy:
         self.s3_resource, self.s3 = get_s3(ASPIRE_AWS_ACCESS_KEY_ID, ASPIRE_AWS_SECRET_ACCESS_KEY)
         self.bucket = self.s3_resource.Bucket(bucket_name)
         self.backup_target = backup_target
-        self.cluster, self.backup_info = None, None
-        for cluster in get_crunchy_clusters():
-            if cluster["name"] != cluster_name:
-                continue
-            self.cluster = cluster
-            self.backup_info = get_cluster_backup_info(cluster["id"])
-            break
-        if self.cluster is None:
-            raise CantFindCrunchyBridgeCluster("Could not find cluster with the given name")
+        self.cluster = self.get_cluster(cluster_name)
+        self.backup_info = get_cluster_backup_info(
+            self.cluster["id"], backup_target=self.backup_target
+        )
+        self.dry_run = dry_run
 
-    def _copy_paths(self, recursive_paths, file_paths):
+    @classmethod
+    def get_cluster(cls, cluster_name: str) -> dict:
+        """Find the cluster from the CrunchyBridge API with the given name"""
+        for cluster in get_crunchy_clusters():
+            if cluster["name"] == cluster_name:
+                return cluster
+        raise CantFindCrunchyBridgeCluster("Could not find cluster with the given name")
+
+    @classmethod
+    def s3_copy_command(cls, src, dest, relative_path) -> str:
+        command = f"aws s3 cp {src}{relative_path} {dest}{relative_path}"
+        if relative_path.endswith("/"):
+            # Any directory needs to be recursive
+            command += " --recursive"
+        return command
+
+    def _copy_paths(self, file_paths: list[str]):
+        """
+        This downloads the files from the CrunchyBridge S3 to a local directory,
+        then uploads them to our S3 bucket.
+
+        :param file_paths: A list of relative file paths. If the path ends with
+                           a `/`, it will be treated as a directory and its
+                           contents will be copied recursively.
+        """
         crunchy_s3_path = (
             f's3://{self.backup_info["aws"]["s3_bucket"]}/'
             f'{self.backup_info["cluster_id"]}/{self.backup_info["stanza"]}'
         )
         download_path = f"{LOCAL_TEMP_DOWNLOADS_PATH}{self.cluster['name']}"
-
-        command_lists = [
-            [
-                "aws",
-                "s3",
-                "cp",
-                f"{crunchy_s3_path}{path}",
-                f"{download_path}{path}",
-                "--recursive",
-            ]
-            for path in recursive_paths
-        ] + [
-            [
-                "aws",
-                "s3",
-                "cp",
-                f"{crunchy_s3_path}{path}",
-                f"{download_path}{path}",
-            ]
-            for path in file_paths
-        ]
+        dest_s3_path = f"{S3_BACKUP_DEST_PREFIX}{self.cluster['name']}/{self.backup_target}"
 
         download_env = {
             "AWS_ACCESS_KEY_ID": self.backup_info["aws"]["s3_key"],
@@ -203,73 +223,83 @@ class CrunchyCopy:
             "AWS_SESSION_TOKEN": self.backup_info["aws"]["s3_token"],
         }
 
-        for i, command_list in enumerate(command_lists):
-            command = " ".join(command_list)
+        if self.dry_run:
+            print("Dry run only.")
+            print(f"Downloading from: {crunchy_s3_path}")
+            print(f"Downloading to: {download_path}")
+            print(f"Uploading to: {dest_s3_path}")
+            print("Commands: \n")
 
-            download_process = subprocess.Popen(
-                command,
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE,
-                universal_newlines=True,
-                env=download_env,
-                shell=True,
+        for i, filepath in enumerate(file_paths):
+            command = self.s3_copy_command(
+                src=crunchy_s3_path,
+                dest=download_path,
+                relative_path=filepath,
             )
 
-            watch_process_logs(download_process)
-            print(f"{i + 1} / {len(command_lists)} downloads complete! Proceeding to upload...")
+            if self.dry_run:
+                print(command)
+            else:
+                download_process = subprocess.Popen(
+                    command,
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.PIPE,
+                    universal_newlines=True,
+                    env=download_env,
+                    shell=True,
+                )
 
-            upload_all_files_in_dir(
-                download_path,
-                self.backup_info["stanza"],
-                self.bucket,
-                self.cluster["name"],
-                BASE_S3_PREFIX,
-            )
-            delete_all_files_in_dir(download_path)
+                watch_process_logs(download_process)
+                print(f"{i + 1} / {len(file_paths)} downloads complete! Proceeding to upload...")
 
-    def _should_process_backup(self, backup: dict):
-        return not self.backup_target or self.backup_target == backup["name"]
+                upload_all_files_in_dir(download_path, self.bucket, prefix=dest_s3_path)
+                delete_all_files_in_dir(download_path)
 
     def _get_copy_paths(self):
-        crunchy_backup_prefix = f"/backup/{self.backup_info['stanza']}"
+        """
+        Get the relative paths of files we need to copy from CrunchyBridge to S3.
 
-        recursive_path_suffixes = [
-            "/archive",
-            f"{crunchy_backup_prefix}/backup.history",
-        ]
-        file_path_suffixes = [
-            f"{crunchy_backup_prefix}/backup.info",
-            f"{crunchy_backup_prefix}/backup.info.copy",
-        ]
+        We need to store the following files and folders as a snapshot in our
+        S3 bucket. Only the files and folders listed in below should be copied.
+        There are  =many other files in the structure that we don't need.
+        Since we will be storing this for years we want to only store what's
+        needed. For example, the archive/ folder has the entire WAL for the
+        last 10 days. We don't need that (as long as CB is using
+        ``--copy-archive``) because it's stored in the pg_data/pg_wal/ folder.
 
-        # Determine if there are any new CrunchyBridge backups to move
-        has_new_backup = False
-        for backup in self.backup_info["backups"]:
-            if not self._should_process_backup(backup):
-                continue
-            if not backup_exists(
-                self.s3,
-                self.bucket,
-                self.cluster["name"],
-                self.backup_info["stanza"],
-                backup["name"],
-            ):
-                has_new_backup = True
-                print(
-                    f"{self.cluster['name']}: Backup {backup['name']} not found in AspirEDU "
-                    f"Bucket... Adding to download list!"
-                )
-                recursive_path_suffixes.append(f"{crunchy_backup_prefix}/{backup['name']}")
-        if not has_new_backup:
-            print("No new backups found!! Exiting script :)")
-            return [], []
-        return recursive_path_suffixes, file_path_suffixes
+        This is a description of the file structure of a CrunchyBridge backup.
+
+        cluster/
+        ├─ archive/  # This is the WAL that is stored while the backup is created
+        │   └─ backup_stanza/
+        │      └─ archive.info
+        └─ backup/  # This is the physical backup created each day
+           └─ backup_stanza/
+              ├─ backup.history/
+              │  └─ */*
+              ├─ backup.info
+              ├─ backup.info.copy
+              └─ 20230101-010000F/
+                 └─ */*
+        """
+
+        stanza = self.backup_info["stanza"]
+        backup_slug = self.backup_info["backup"]["name"]
+        files_to_copy = [
+            f"/archive/{stanza}/archive.info",
+            f"/backup/{stanza}/backup.info",
+            f"/backup/{stanza}/backup.copy",
+            # recursive folders
+            f"/backup/{stanza}/backup.history/",
+            f"/backup/{stanza}/{backup_slug}/",
+        ]
+        return files_to_copy
 
     def process(self):
         script_start = datetime.utcnow().replace(tzinfo=TZ)
 
-        recursive_paths, file_paths = self._get_copy_paths()
-        self._copy_paths(recursive_paths, file_paths)
+        file_paths = self._get_copy_paths()
+        self._copy_paths(file_paths)
 
         summarize(
             script_start,
@@ -303,20 +333,28 @@ def main():
     parser.add_argument(
         "-c",
         "--cluster",
-        required=False,
-        help="(Optional) The name of the database cluster to pretend to create",
+        required=True,
+        help="The name of the database cluster to pretend to create",
     )
     parser.add_argument(
         "-t",
         "--target",
-        help="(Optional) The name of a specific backup to target otherwise targets today's backups.",
+        required=False,
+        help="(Optional) The name of a specific backup to target. Defaults to today's backups.",
+    )
+    parser.add_argument(
+        "--dry-run",
+        action="store_true",
+        help="(Optional) This won't download or upload the files, instead it will print the paths.",
     )
     args = parser.parse_args()
     bucket_name = (
         "aspiredu-pgbackups" if args.cluster not in AU_BACKENDS else "aspiredu-pgbackups-au"
     )
     backup_target = args.target or datetime.now().strftime("%Y%m%d")
-    CrunchyCopy(bucket_name, args.cluster, backup_target=backup_target).process()
+    CrunchyCopy(
+        bucket_name, args.cluster, backup_target=backup_target, dry_run=args.dry_run
+    ).process()
     exit(0)
 
 
